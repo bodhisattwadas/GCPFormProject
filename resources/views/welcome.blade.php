2ebf9b5ed3cd1f7866fd778710d4d870b9d66de1
bodhisattwa das <das.bodhisattwa@gmail.com>
2023-09-07 19:30:59 +0530

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -376,10 +376,42 @@
 
                 
             </form>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <p id="modalBody"></p>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
     </div>
     
     <script>
-        
+        //onsubmit show all details in bootstrap modal
+         $(document).ready(function(){
+            $('form').on('submit', function(e){
+                e.preventDefault();
+                //get all form data
+                var data = $(this).serialize();
+                //show bootstrap modal
+                $('#exampleModal').modal('show');
+                //show form data in modal body
+
+                $('#modalBody').html(data);
+            });
+        });
         $(
             function(){
                 $('#children').on('change', function(){
